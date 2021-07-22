80d97a99a525077994afa422acfdd2730765e746
Itz-Hyperz <69090660+Itz-Hyperz@users.noreply.github.com>
2021-07-22 10:17:02 -0700

    Update main.js

--- main.js
+++ main.js
@@ -44,38 +44,9 @@ class buddy {
     }
 
     async webhookError(error, webhookId, webhookToken) {
-        async function consoleError(error, color) {
-            let logcolor;
-            switch (color) {
-                case 'blue':
-                    logcolor = chalk.blue(error);
-                    break;
-                case 'green':
-                    logcolor = chalk.green(error);
-                    break;
-                case 'yellow':
-                    logcolor = chalk.yellow(error);
-                    break;
-                case 'yello':
-                    logcolor = chalk.yellow(error);
-                    break;
-                case 'red':
-                    logcolor = chalk.red(error);
-                    break;
-                case 'black':
-                    logcolor = chalk.black(error);
-                    break;
-                case 'white':
-                    logcolor = chalk.white(error);
-                    break;
-                default:
-                    logcolor = chalk.red(error);
-            }
-            console.log(`\n --- NodeBuddy Error --- \n\n${logcolor}\n`);
-        }
         if(this.useDiscord) {
-            if(!this.discordUsername) return consoleError(`Not all Discord information is provided in your constructor.`, `yellow`);
-            if(!this.discordAvatarURL) return consoleError(`Not all Discord information is provided in your constructor.`, `yellow`);
+            if(!this.discordUsername) return console.log(chalk.yellow(`Not all Discord information is provided in your constructor.`));
+            if(!this.discordAvatarURL) return console.log(chalk.yellow(`Not all Discord information is provided in your constructor.`));
 
             const webhookClient = new Discord.WebhookClient(webhookId, webhookToken);
 
@@ -90,10 +61,10 @@ class buddy {
                 avatarURL: this.discordAvatarURL,
                 embeds: [embed]
             }).catch(e => {
-                consoleError(`\nWebhook Error: I was unable to send the embeded error message to the webhook || ID: ${webhookId} || TOKEN: ${webhookToken}\n\n${e}\n`, `red`);
+                console.log(chalk.red(`\nWebhook Error: I was unable to send the embeded error message to the webhook || ID: ${webhookId} || TOKEN: ${webhookToken}\n\n${e}\n`));
             });
         } else {
-            consoleError('\nuseDiscord is disabled or not defined in your constructor.\n', 'yellow');
+            console.log(chalk.yellow('\nuseDiscord is disabled or not defined in your constructor.\n'));
         }
     }
 
